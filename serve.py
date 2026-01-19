413c30466fc2e19a637ae12c2e2f3fecf7da223c
sayak samanta <sayaksmnt@gmail.com>
2026-01-19 12:47:51 +0530

    Update serve.py

--- serve.py
+++ serve.py
@@ -6,7 +6,7 @@ app = create_app()
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
     debug = app.config['DEBUG']
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
 else:
 
     gunicorn_app = app
